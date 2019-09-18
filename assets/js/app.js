3a7d07956aa59b91be534835799f98f74c7fe659
arcela43 <arcela43@gmail.com>
2019-09-17 22:09:44 -0400

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,201 @@
+const app = angular.module('app', ['ui.materialize', 'ngRoute']);
+
+app.config(function ($locationProvider, $routeProvider) {
+	$routeProvider
+		.when("/", {
+			// templateUrl: "./pages/home.html",
+			templateUrl: "./pages/contest2.html",
+			controller: "App"
+		})
+		.when("/contest", {
+			templateUrl: "./pages/contest2.html",
+			controller: "App"
+		})
+		.when("/top", {
+			templateUrl: "pages/top/top_template.html",
+			controller: "App"
+		})
+		.when("/evaluation", {
+			templateUrl: "pages/evaluation/evaluation_template.html"
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+});
+
+app.controller('App',
+	["$scope", "$location", "$http", "$interval", "$timeout",
+		function ($scope, $location, $http, $interval, $timeout, $sce) {
+
+			$scope.state = {
+				lastModified: null,
+				isModified: false,
+				sheetsNames: [],
+				sheets: [],
+				book: {}
+			}
+
+			$scope.curretCategory = ""
+			$scope.showCategory = false
+			
+
+			$scope.isActive = (viewLocation) => {
+				return viewLocation === $location.path();
+			};
+
+			$scope.goToContest = () => {
+				$location.path('/contest')
+			}
+			$scope.goBack = () => {
+				$scope.showCategory = false
+				$scope.topSheet = null			
+			}
+
+			$scope.getData = () => {
+
+				$http({
+						method: 'GET',
+						url: 'assets/data/timbaland2019.xlsx',
+						responseType: 'arraybuffer',
+						headers: {
+							'Cache-control': 'no-cache'
+						},
+					})
+					.then(
+						(data) => {
+
+							$scope.state.lastModified = data.headers('Last-Modified');
+							$scope.state.isModified = true;
+							$timeout(
+								() => {
+									$scope.state.isModified = false;
+									$scope.state.book = XLSX.read(data.data, {
+										type: "array"
+									});
+									$scope.state.sheetsNames = $scope.state.book.SheetNames;
+									for (i = 0; i < Object.keys($scope.state.book.Sheets).length; i += 1) {
+										$scope.state.sheets[i] = XLSX.utils.sheet_to_json($scope.state.book.Sheets[$scope.state.book.SheetNames[i]]);
+									}
+								},
+								1000
+							);
+							$scope.transformToObject()
+
+						},
+						(err) => {
+							console.log(err);
+						}
+					);
+			}
+			
+			$scope.reloadData = () => {
+
+				$http({
+						method: 'GET',
+						url: 'assets/data/timbaland2019.xlsx',
+						responseType: 'arraybuffer',
+						headers: {
+							'Cache-control': 'no-cache'
+						},
+					})
+					.then(
+						(data) => {
+
+							// console.log(data)
+
+							// if (data.headers('Last-Modified') !== $scope.state.lastModified) {
+							$scope.state.lastModified = data.headers('Last-Modified');
+							$scope.state.isModified = true;
+							$scope.state.book = XLSX.read(data.data, {
+								type: "array"
+							});
+							$timeout(
+								() => {
+									$scope.state.isModified = false;
+									for (i = 0; i < Object.keys($scope.state.book.Sheets).length; i += 1) {
+										$scope.state.sheets[i] = XLSX.utils.sheet_to_json($scope.state.book.Sheets[$scope.state.book.SheetNames[i]]);
+									}
+								},
+								1500
+
+							);
+							$scope.transformToObject()
+
+							// }
+						},
+						(err) => {
+							console.log(err);
+						}
+					);
+			}
+
+			$scope.getData();
+			$interval($scope.getData, 5000);
+
+
+			$scope.goToTop = (index, currentCategory, sheet) => {
+				// debugger
+				$scope.showCategory = true
+				$scope.currentCategory = currentCategory
+				$scope.index = index
+				$scope.topSheet = sheet
+				$scope.transformToObject($scope.topSheet)
+
+			}
+
+			$scope.transformToObject = (sheet) => {
+
+				if ($scope.topSheet != null) {
+
+					$scope.top = []
+					$scope.header = []
+					$scope.state.sheets[$scope.index].map((data, index) => {
+
+						if (index === 0) {
+							$scope.header.push({
+								academia: data.__EMPTY,
+								participante: data.__EMPTY_1,
+								total: data.__EMPTY_2,
+								item_1: data.__EMPTY_4,
+								item_2: data.__EMPTY_3,
+								item_3: data.__EMPTY_5,
+								item_4: data.__EMPTY_6,
+								item_5: data.__EMPTY_7,
+								item_6: data.__EMPTY_8,
+								item_7: data.__EMPTY_9,
+								item_8: data.__EMPTY_10,
+								item_9: data.__EMPTY_11,
+								item_1: data.__EMPTY_12,
+
+							})
+						} else {
+							$scope.top.push({
+									academia: data.__EMPTY,
+									participante: data.__EMPTY_1,
+									total: data.__EMPTY_2,
+									item_1: data.__EMPTY_4,
+									item_2: data.__EMPTY_3,
+									item_3: data.__EMPTY_5,
+									item_4: data.__EMPTY_6,
+									item_5: data.__EMPTY_7,
+									item_6: data.__EMPTY_8,
+									item_7: data.__EMPTY_9,
+									item_8: data.__EMPTY_10,
+									item_9: data.__EMPTY_11,
+									item_1: data.__EMPTY_12,
+								}
+
+
+							)
+						}
+
+					})
+					console.log("DATA", $scope.top)
+				}
+			}
+		}
+	]);
