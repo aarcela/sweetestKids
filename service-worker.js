bf0e4a245a920d5ef5f77026b4ac77ad9c71a072
arcela43 <arcela43@gmail.com>
2019-10-07 10:15:01 -0400

    runtime cache

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v2';
-const RUNTIME = 'runtime';
+const RUNTIME = 'runtime-v2';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
